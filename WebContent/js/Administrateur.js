23361ec67aa6462dfd3123c96ce6d0270bded244
charmallet <charlesmallet10@hotmail.fr>
2018-10-28 10:52:54 +0100

    Pop Up done

---
+++ WebContent/js/Administrateur.js
@@ -0,0 +1,13 @@
+
+
+	
+	function select_value(){
+		 window.open("test.html",'nom_de_ma_popup','menubar=no, scrollbars=no, top=100, left=100, width=500, height=500');
+		 return document.getElementById("typePage").value;
+			 
+		}
+		
+		
+
+	
+	
---
+++ WebContent/js/test.js
@@ -0,0 +1,12 @@
+
+
+function init (){
+	//alert("coucou");
+	//alert(select_value());
+	
+	$("#container").load("Administrateur.html");
+	//$("#container").append('hello');
+	
+	//window.open("test.html",'nom_de_ma_popup','menubar=no, scrollbars=no, top=100, left=100, width=500, height=500');
+
+}
