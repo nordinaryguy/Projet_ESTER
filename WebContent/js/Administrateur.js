319a69cb1089e464b07b7a552caf25222cbb9039
charmallet <charlesmallet10@hotmail.fr>
2018-11-15 17:11:11 +0100

    test.html Renamed into Interface.html

--- WebContent/js/Administrateur.js
+++ WebContent/js/Administrateur.js
@@ -1,6 +1,6 @@
 
 	function select_value(){		
-		window.open("test.html?type="+document.getElementById("typePage").value,"Nouvelle fenêtre","menubar=no, scrollbars=no, top=100, left=100, width=1000, height=800");
+		window.open("Interface.html?type="+document.getElementById("typePage").value,"Nouvelle fenêtre","menubar=no, scrollbars=no, top=100, left=100, width=1000, height=800");
 	}
 	
 	
